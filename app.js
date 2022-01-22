23854bdadbd625cbf989f666c18d919d1a6fae38
VEXG <helmiaryanto54@gmail.com>
2022-01-22 23:27:08 +0700

    optimize

--- app.js
+++
@@ -1,9 +0,0 @@
-try {
-    new (require('./lib/functions.js'))().run();
-    process.on('uncaugtException', console.error);
-} catch (e) {
-    console.error({
-        e,
-        path: __dirname
-    });
-}
