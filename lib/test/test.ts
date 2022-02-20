32033403f4c111493eb7c2dedd585002d3e790f2
VEXG <helmiaryanto54@gmail.com>
2022-02-20 19:45:15 +0700

    removing unused things

--- lib/test/test.ts
+++
@@ -1,8 +0,0 @@
-import { parseRegex } from '../utilities';
-import i from '../../src/cilok.config.json';
-
-const a = i.prefix.map((a) =>
-	(a as any) instanceof RegExp ? a : new RegExp(`^(${parseRegex(a)})`, 'i'),
-);
-
-console.log(a);
